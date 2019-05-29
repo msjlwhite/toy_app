592f71a83df061c71cf9af712d45d2d29304b181
J White <msjlwhite@gmail.com>
2019-05-29 17:08:11 -0500

    trying to deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+=begin
+Listing 2.2: Adding a hello action to the Application controller.
+app/controllers/application_controller.rb
+=end
+
+
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+  
+    def hello
+      render html: "hello, world!"
+    end
+  end
